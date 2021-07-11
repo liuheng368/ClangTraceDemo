9e4c4aa9f9bedff912a054edf871154fc253d919
Henry <lhappledev@163.com>
2021-07-11 12:54:56 +0800

    first commit

---
+++ test/ClangTools.h
@@ -0,0 +1,16 @@
+//
+//  ClangTools.h
+//  test
+//
+//  Created by Henry on 2021/7/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClangTools : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/ClangTools.m
@@ -0,0 +1,12 @@
+//
+//  ClangTools.m
+//  test
+//
+//  Created by Henry on 2021/7/11.
+//
+
+#import "ClangTools.h"
+
+@implementation ClangTools
+
+@end
